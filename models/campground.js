878df784de8897e865a7d949502545e5c9ac88fb
ashutosh8013 <aj.ashutoshjha2000@gmail.com>
2024-09-22 00:17:56 +0530

    thumbnail is still pending

--- models/campground.js
+++ models/campground.js
@@ -1,6 +1,13 @@
+const { func } = require("joi");
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
+
+const ImageSchema = new Schema({ url: String, filename: String });
+
+ImageSchema.virtual('thumbnail').get(function(){
+  return this.url.replace('/upload','/upload/w_200');
+})
 
 const CampgroundSchema = new Schema({
   title: String,
@@ -9,12 +16,7 @@ const CampgroundSchema = new Schema({
     ref: "User",
   },
 
-  images: [
-    {
-      url: String,
-      filename: String,
-    },
-  ],
+  images: [ImageSchema],
   price: Number,
   description: String,
   location: String,
